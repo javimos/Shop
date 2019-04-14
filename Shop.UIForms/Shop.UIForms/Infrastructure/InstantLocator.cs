cad1d0adb41a9fdab17a796f879b056d687b312a
Javier Martínez <javimos@gmail.com>
2019-04-14 21:08:01 +0200

    Empezamos con Android

---
+++ Shop.UIForms/Shop.UIForms/Infrastructure/InstantLocator.cs
@@ -0,0 +1,14 @@
+﻿namespace Shop.UIForms.Infrastructure
+{
+    using ViewModels;
+
+    public class InstantLocator
+    {
+        public MainViewModel Main { get; set; }
+
+        public InstantLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+    }
+}
